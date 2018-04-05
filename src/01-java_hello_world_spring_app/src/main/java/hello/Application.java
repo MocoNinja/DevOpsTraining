7103cf5141c0a39feaa00673eb5078e2a0a31aac
MocoNinja <javiergonzaleztello@gmail.com>
2018-04-05 13:36:09 +0200

    Hello World application working inside Eclipse
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

--- src/01-java_hello_world_spring_app/src/main/java/hello/Application.java
+++ src/01-java_hello_world_spring_app/src/main/java/hello/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Starting sample Java web application with correctly configured Eclipse and Maven...")
+        System.out.println("Starting sample Java web application with correctly configured Eclipse and Maven...");
         SpringApplication.run(Application.class, args);
     }
 
