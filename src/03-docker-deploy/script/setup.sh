9e6d7ed08846378bb6b4d00842917d276980ceb0
MocoNinja <javiergonzaleztello@gmail.com>
2018-04-09 14:14:50 +0200

    Corrected script file: uploaded was not ftp'ed correctly
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

--- src/03-docker-deploy/script/setup.sh
+++ src/03-docker-deploy/script/setup.sh
@@ -7,3 +7,14 @@ if [ ! -d /apps/bin ]; then
 fi
 
 echo "Directories created!"
+
+echo "Exporting environment variables..."
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+echo "JAVA_HOME SET TO $JAVA_HOME!"
+
+echo "Adding JAVA to path..."
+export PATH=$JAVA_HOME/bin:$PATH
+echo "Path updated!"
+
+echo $PATH
+echo "Finished!"
