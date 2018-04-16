92cc999139edbf11ce99767dd4914e5c200d06d0
MocoNinja <javiergonzaleztello@gmail.com>
2018-04-16 14:14:03 +0200

    Exito
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

--- src/05-Spring-MongoDB-App/create.sh
+++ src/05-Spring-MongoDB-App/create.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
-DBPATH='/home/centos/docker/persistence/mongodb/spring-sample-app'
-NETWORKNAME='spring_demo_net'
+DBPATH='/home/centos/docker/persistence/mongodb/spring-sample-app-customete'
+DB_TABLE_NAME='test'
+NETWORKNAME='net1'
 PORT=45000
-JAVA_APP='spring-guestbook-mongo'
+JAVA_IMAGE='spring-guestbook'
+JAVA_CONTAINER='spring-guestbook'
+MONGO_CONTAINER='spring-mongo'
 
 echo "============================"
 echo "= Stating Spring Mongo APP ="
@@ -23,12 +26,24 @@ fi
 
 echo "Starting a mongo instance..."
 echo "(USING OFFICIAL IMAGE)"
-sudo docker run --name spring-demo-mongo --network=$NETWORKNAME -v $DBPATH:/data/db -d mongo
+sudo docker run --name $MONGO_CONTAINER --network=$NETWORKNAME -v $DBPATH:/data/db -d mongo
+
+echo "Writing custom Dockerfile..."
+if [ -f Dockerfile ]; then
+	echo "Dockerfile already present. Overriding..."
+	rm Dockerfile
+fi
+
+/bin/cat <<EOM >Dockerfile
+FROM openjdk:8-jdk
+ADD app.war /app.war
+EXPOSE 8080
+ENTRYPOINT ["java", "-Dspring.data.mongodb.uri=mongodb://$MONGO_CONTAINER/$DB_TABLE_NAME", "-jar","/app.war"]
+EOM
 
 echo "Compiling the image of the app..."
-cd java-app
-sudo docker build -t $JAVA_APP .
+sudo docker build -t $JAVA_IMAGE .
 cd ..
 
 echo "Stating app instance..."
-sudo docker run -d --name spring-demo-app --network=$NETWORKNAME -p $PORT:8080  $JAVA_APP
+sudo docker run -d --name $JAVA_CONTAINER --network=$NETWORKNAME -p $PORT:8080  $JAVA_IMAGE
