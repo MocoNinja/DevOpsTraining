424d15e3796aa7a341009c53dd01ba912311bcd3
MocoNinja <javiergonzaleztello@gmail.com>
2018-05-16 14:50:25 +0000

    Ale****ingluyah: logging into a file!
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

--- DockerFiles/14-fluentd-learn/runFluent.sh
+++ DockerFiles/14-fluentd-learn/runFluent.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# sudo docker run -d -v $(pwd)/data:/etc/fluentd/log 
-sudo docker run -it -p 24224:24224 --network="fluent-test" fluentd-custom
+sudo docker run -it -p 24224:24224 -v $(pwd)/logs:/fluentd/log --network="fluent-test" fluentd-custom
