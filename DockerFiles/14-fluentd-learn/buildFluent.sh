984cbc3414779dbc3f4643140161d0b51202d7b3
MocoNinja <javiergonzaleztello@gmail.com>
2018-05-17 07:31:27 +0000

    Removed logs from git and updated scripts to push the images to registry
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

--- DockerFiles/14-fluentd-learn/buildFluent.sh
+++ DockerFiles/14-fluentd-learn/buildFluent.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sudo docker build -t fluentd-custom -f fluentd.dockerfile .
+
+sudo docker tag fluentd-custom 192.168.2.18:5000/fluentd-custom:v1
+
+sudo docker push 192.168.2.18:5000/fluentd-custom:v1
--- DockerFiles/14-fluentd-learn/buildLogger.sh
+++ DockerFiles/14-fluentd-learn/buildLogger.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sudo docker build -t logger-app -f loggerApp.dockerfile .
+
+sudo docker tag logger-app 192.168.2.18:5000/logger:v1
+
+sudo docker push 192.168.2.18:5000/logger:v1
